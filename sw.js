68c8e858d73427f3ec7e659f34f3180238c38e49
dfluch-hub <d.fluch@gmx.at>
2026-09-06 13:18:31 +0200

    Ensure onboarding spacing reaches mobile PWA

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="aftercare-os-v5-1";
+const CACHE="aftercare-os-v5-2";
 const ASSETS=["./","./index.html","./assets/app.css","./js/config.js","./js/storage.js","./js/state.js","./js/ui.js","./js/app.js","./js/commerce.js","./sales.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
