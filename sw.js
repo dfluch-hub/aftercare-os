96f57ea24643996a35d57284ae6c2dcc5abcaeb5
dfluch-hub <d.fluch@gmx.at>
2026-09-07 23:42:17 +0200

    Refresh PWA cache for senior Today rebuild

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'aftercare-pathway-v6';
+const CACHE = 'aftercare-pathway-v7';
 const ASSETS = ['./', './index.html', './assets/recovery.css', './assets/recovery-hero.jpg', './js/recovery.js', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', event => {event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));});
 self.addEventListener('activate', event => {event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key.startsWith('aftercare-') && key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim()));});
