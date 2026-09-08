5cf8af84e6dbe3db2bda6412f23f22be683ada35
dfluch-hub <d.fluch@gmx.at>
2026-09-08 10:21:20 +0200

    Refresh RecoverOS PWA cache for v21 rebuild

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='aftercare-v20';
-const ASSETS=['./','./index.html','./assets/recovery-hero.jpg','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+const CACHE='recoveros-v21';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET'||new URL(e.request.url).origin!==self.location.origin)return;e.respondWith(fetch(e.request).then(r=>{if(r.ok&&ASSETS.some(p=>new URL(p,self.registration.scope).href===e.request.url)){const copy=r.clone();e.waitUntil(caches.open(CACHE).then(c=>c.put(e.request,copy)))}return r}).catch(()=>caches.match(e.request).then(c=>c||(e.request.mode==='navigate'?caches.match(new URL('./index.html',self.registration.scope)):Response.error()))))});
