5552cd2f81a5f306797335ed5e9ec5ca080e5060
dfluch-hub <d.fluch@gmx.at>
2026-09-11 17:07:12 +0200

    Bump cache for four-tab settings sheet hotfix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='mend-v1.0.1';
+const CACHE_NAME='mend-v1.0.2';
 const CORE=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
